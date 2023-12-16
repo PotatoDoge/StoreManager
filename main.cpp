0fcf4d628742ddeb4e5fb29a0b87db47bcc12593
bprado <bastian.prado.v@gmail.com>
2023-12-16 12:46:51 -0800

    Move item to folder

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <list>
-#include "entity/Item.h"
+#include "entity/Item/Item.h"
 
 using namespace std;
 
@@ -9,10 +9,10 @@ int main() {
     Item fridge("Fridge",35.5,"Brand new fridge");
     Item oven("Oven",20.0,"Oven");
     Item lamp("Lamp",5.0,"Lamp");
-    list<Item> ikea = {woodenChair,fridge,oven,lamp};
+    list<Item> shoppingCart = {woodenChair,fridge,oven,lamp};
 
-    for (Item elem : ikea) {
-        elem.showItemInfo();
+    for (Item item : shoppingCart) {
+        item.showItemInfo();
         cout << "-----------" << endl;
     }
     return 0;
